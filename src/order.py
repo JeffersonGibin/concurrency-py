81cdfe8ba81af68c22c0edd2b7d2151b0e0f4e31
Jefferson Gibin <jefferson@jefferson.gibin>
2023-02-15 09:52:20 -0300

    Refactory in the Order class

--- src/order.py
+++ src/order.py
@@ -4,18 +4,18 @@ import math
 import random
 
 class Order:
-    def __init__(self, dados: dict) -> None:
-        self.numero_pedido = math.ceil(random.randint(1, 999999))
-        self.data = datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-        self.nome =  dados.get("client_name"),
-        self.pedido = dados.get("order_itens"),
+    def __init__(self, data: dict) -> None:
+        self.random_order_number = math.ceil(random.randint(1, 999999))
+        self.date = datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        self.client_name =  data.get("client_name"),
+        self.order_itens = data.get("order_itens"),
 
     def get(self):
-        nome, = self.nome
+        client_name, = self.client_name
         return {
-            "order_number": self.numero_pedido,
-            "date": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-            "order_itens": self.pedido,
-            "client_name": nome,
+            "order_number": self.random_order_number,
+            "date": self.date,
+            "order_itens": self.order_itens,
+            "client_name": client_name,
         }
 
