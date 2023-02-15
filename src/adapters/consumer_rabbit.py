9aa47a34186ae13ac1db406cb0c6c285d4ceacf5
Jefferson Gibin <jefferson@jefferson.gibin>
2023-02-15 11:06:04 -0300

    Adjusted adapters

--- src/adapters/consumer_rabbit.py
+++ src/adapters/consumer_rabbit.py
@@ -1,5 +1,5 @@
 import json
-from pika import PlainCredentials, BlockingConnection, ConnectionParameters
+from pika import PlainCredentials, BlockingConnection, ConnectionParameters, exceptions
 
 from src.interfaces.ConsumerInterface import Consumer
 
@@ -8,13 +8,17 @@ class ConsumerRabbitMQ(Consumer):
         self.settings = settings
 
     def consumer(self, cb):
-        credentials = PlainCredentials('rabbitmq', 'rabbitmq')
-        connection = BlockingConnection(ConnectionParameters(host='rabbit', port=5672, credentials=credentials))
-        channel = connection.channel()
-    
-        queue_name = self.settings.get("queue")
-        channel.queue_declare(queue=queue_name)
-        
-        channel.basic_consume(queue=self.settings.get("queue"), on_message_callback=cb, auto_ack=True)
+        try:
+            credentials = PlainCredentials('rabbitmq', 'rabbitmq')
+            connection = BlockingConnection(ConnectionParameters(host='rabbit', port=5672, credentials=credentials, socket_timeout=10))
+            channel = connection.channel()
 
-        channel.start_consuming()
+            queue_name = self.settings.get("queue")
+            channel.queue_declare(queue=queue_name)
+            
+            channel.basic_consume(queue=self.settings.get("queue"), on_message_callback=cb, auto_ack=True)
+
+            channel.start_consuming()
+            
+        except exceptions.AMQPConnectionError as err:
+            print("Could not establish connection to RabbitMQ:", err)
--- src/adapters/producer_rabbit.py
+++ src/adapters/producer_rabbit.py
@@ -1,5 +1,5 @@
 import json
-from pika import PlainCredentials, BlockingConnection, ConnectionParameters
+from pika import PlainCredentials, BlockingConnection, ConnectionParameters, exceptions
 
 from src.interfaces.ProducerInterface import Producer
 
@@ -8,18 +8,25 @@ class ProducerRabbitMQ(Producer):
         self.settings = settings
 
     def publish(self, payload: dict):
-         # Conecte-se ao servidor RabbitMQ
-        credentials = PlainCredentials('rabbitmq', 'rabbitmq')
-        connection = BlockingConnection(ConnectionParameters(host='rabbit', port=5672, credentials=credentials))
-        channel = connection.channel()
+
+        try:
+            # Conecte-se ao servidor RabbitMQ
+            credentials = PlainCredentials('rabbitmq', 'rabbitmq')
+            connection = BlockingConnection(ConnectionParameters(host='rabbit', port=5672, credentials=credentials, socket_timeout=10))
+            channel = connection.channel()
+            
         
-        # Declaração da Fila de Pedidos
-        queue_name = self.settings.get("queue")
-        channel.queue_declare(queue=queue_name)
-        
-        # Envia o Pedido para a Fila
-        payload= json.dumps(payload)
-        channel.basic_publish(exchange='',
-                            routing_key=self.settings.get("routing_key"),
-                            body=payload)
-        connection.close()
+            if channel:
+                # Declaração da Fila de Pedidos
+                queue_name = self.settings.get("queue")
+                channel.queue_declare(queue=queue_name)
+            
+                # Envia o Pedido para a Fila
+                payload= json.dumps(payload)
+                channel.basic_publish(exchange='',
+                                    routing_key=self.settings.get("routing_key"),
+                                    body=payload)
+                connection.close()
+
+        except exceptions.AMQPConnectionError as err:
+            print("Could not establish connection to RabbitMQ:", err)
